adfa52622c062c546aa1890dab0a411cc5ebd91f
jibarozzo <bolaponte@proton.me>
2024-07-10 09:19:13 -0500

    Renamed extensions and .cls files after much modification. New _extension/tulane-thesis. Delete unused files.

--- lua/pagenumber.lua
+++
@@ -1,7 +0,0 @@
--- page_number_upper_right.lua
-function Header(el)
-  if el.level == 1 then
-    table.insert(el.content, pandoc.RawInline('latex', '\\thispagestyle{scrheadings}'))
-  end
-  return el
-end
